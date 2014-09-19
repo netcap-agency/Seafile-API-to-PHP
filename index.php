389c27d5ab79a3b149b440661ce19f4fb0e84537
peacepostman <ben@netcap.fr>
2014-09-19 13:23:24 +0200

    Remove useless script
    Remove ie10-viewport-bug-workaround.js from exemple

--- index.php
+++ index.php
@@ -144,7 +144,5 @@ $seafile = new SeafileAPI(array(
 		?> 
 	</div> 
 	<!-- /container -->
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
